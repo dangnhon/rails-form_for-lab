f56b6d9494393d98645fcba1b4ec72c51eeeae30
dangnhon <dang.nhon12345@yahoo.com>
2020-12-15 10:06:26 -0800

    MISSING the params prior to require, dang it

--- app/controllers/School_Classes_controller.rb
+++ app/controllers/School_Classes_controller.rb
@@ -23,9 +23,7 @@ class SchoolClassesController < ApplicationController
 
     def update 
         @school_class = SchoolClass.find(params[:id])
-        @school_class.update(title: params[:school_class][:title], room_number: params[:school_class][:room_number])
-        # Code below gives me a type error:  "no implicit conversion of Symbol into String" What does this mean??
-        #@school_class.update(require(:school_class).permit(:title, :room_number))
+        @school_class.update(params.require(:school_class).permit(:title, :room_number))
         redirect_to @school_class
     end 
 end 
